6a2c358c5a0525b7dd32609e58b7f542434ce4ed
rasmil <muhammedrasmil2001@gmail.com>
2024-07-23 20:37:01 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import pandas as pd
 import tempfile
 import os
 import pickle
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 st.title('Document QnA with RAG 💡')
 
 st.info('''Welcome to DocuQ! 📄✨
@@ -20,11 +22,12 @@ if file is not None:
     for pages in pdf_reader.pages:
         text += pages.extract_text()
     st.write(text)
+    embeddings=GoogleGenerativeAIEmbeddings()
+    splitter=RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=20)
+    splitted_text=splitter.split_text(text=text)
+    st.write(splitted_text)
 
-#st.session_state.loader=PyPDFDirectoryLoader('/','.pkl')
-#store_name=file.name[:-4]
-#with open(f'{store_name}.pkl','wb') as f:
-#            pickle.dump(store_name,f)
+
 
 
 
